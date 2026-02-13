dfd87d34727a5f987a8f70167f5e6ad0b3bababc
Rabea <rabea3.kadd@hotmail.com>
2026-02-13 17:20:11 +0300

    removed port from db endpoint

--- Terraform/modules/secret-store/main.tf
+++ Terraform/modules/secret-store/main.tf
@@ -9,7 +9,7 @@ resource "aws_secretsmanager_secret_version" "values" { #populating secret store
   secret_string = jsonencode({
     username = var.db_username
     password = var.db_password
-    host     = var.db_endpoint
+    host     = split(":", var.db_endpoint)[0] #removing port at the end
     port     = var.db_port
     dbname   = var.db_name
   })
